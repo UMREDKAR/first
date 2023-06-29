c6e87dce76f980b6985d4c5be6af7c6c05dfc9ed
UMREDKAR <18bcs133@ietdavv.edu.in>
2023-06-29 16:12:57 +0530

    reactExpansedemo

---
+++ reactExpanse.js
@@ -0,0 +1,11 @@
+function ExpanseItem()
+{
+   return(<div>
+           <h2>Expanse Items</h2>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movie Rs 200</p>
+          </div>)
+}
+
+export default ExpanseItem
