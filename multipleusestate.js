c93bc2f65001177270823548f6216f54bc3412a3
UMREDKAR <18bcs133@ietdavv.edu.in>
2023-07-02 04:32:16 +0530

    multipleusestate

---
+++ multipleusestate.js
@@ -0,0 +1,45 @@
+import React,{useState} from "react";
+
+const ProductForm = () =>
+{
+    const[enteredtitle,setenteredtitle] = useState('');
+    const[enteredAmount,setenteredAmount] = useState('');
+    const[enteredDate ,setenteredDate ] = useState('');
+   
+   const titleChangeHandler = (event)=>
+   {
+      setenteredtitle(event.target.value);
+   }
+   
+   const amountChangeHandler = (event)=>
+   {
+     setenteredAmount(event.target.value);
+   }
+   
+   const dateChangeHandler = (event)=>
+   {
+      setenteredDate(event.target.value);
+   }
+
+   return(
+          <form>
+            <div>
+               <label>Expense Title</label>
+               <input type="text" onChange={titleChangeHandler}></input>
+            </div>
+            <div>
+               <label>Expense Amount</label>
+               <input type="number" onChange={amountChangeHandler}></input>
+            </div>
+            <div>
+                <label>Date</label>
+                <input type="date" min='2023-01-01' max='2023-12-12' onChange={dateChangeHandler}></input>
+            </div>
+            <div>
+                <button type="submit">Add Product</button>
+            </div>
+          </form>
+   );
+}
+
+export default ProductForm;
